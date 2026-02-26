b8d24f98dcf7c797f68d0d61ae15447542ebf4fd
Bernhard Haubold <haubold@evolbio.mpg.de>
2026-02-26 09:02:34 +0100

    Improved setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,6 +9,7 @@ cd neighbors
 bash scripts/setup.sh
 make
 ln -s $(pwd)/bin/* ~/bin/
+cd ..
 # Install datasets
 curl https://ftp.ncbi.nlm.nih.gov/pub/datasets/command-line/v2/linux-amd64/datasets -o ~/bin/datasets
 chmod +x ~/bin/datasets
@@ -18,15 +19,18 @@ cd fur
 bash scripts/sethup.sh
 make
 ln -s $(pwd)/bin/* ~/bin/
+cd ..
 # Install Prim
 git clone https://github.com/evolbioinf/prim
 cd prim
 bash scripts/setup.sh
 make
 ln -s $(pwd)/bin/* ~/bin/
+cd ..
 # Install Biobox
 git clone https://github.com/evolbioinf/biobox
 cd biobox
 bash scripts/setup.sh
 make
 ln -s $(pwd)/bin/* ~/bin/
+cd ..
